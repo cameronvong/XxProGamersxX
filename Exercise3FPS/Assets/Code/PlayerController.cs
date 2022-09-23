1b0ed528906110027b0943bf7a0269bb9d2c6d4a
jx1279 <jx1279@nyu.edu>
2022-09-23 05:18:30 -0400

    Fixed Issue With Looking
    Moving the mouse up and down would move the camera horizontally, while moving the mouse horizontally would move the camera vertically. This issue has been fixed by swapping which input is read for moving the camera in which direction.

--- Exercise3FPS/Assets/Code/PlayerController.cs
+++ Exercise3FPS/Assets/Code/PlayerController.cs
@@ -48,8 +48,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        yRotation += Input.GetAxis("Mouse Y") * lookSpeedX;
-        xRotation += Input.GetAxis("Mouse X") * lookSpeedY; //inverted
+        yRotation += Input.GetAxis("Mouse X") * lookSpeedX;
+        xRotation += Input.GetAxis("Mouse Y") * lookSpeedY; //inverted
         xRotation = Mathf.Clamp(xRotation, -90, 90); //Keeps up/down head rotation realistic
         camTrans.localEulerAngles = new Vector3(xRotation, 0, 0);
         transform.eulerAngles = new Vector3(0, yRotation, 0);
