87d3a300b8bac7ab01d0dd1f15e308d3324b4dc4
jx1279 <jx1279@nyu.edu>
2022-09-30 05:39:10 -0400

    Updated PlayerInteracts To Destroy Specific Doors
    This update should allow specific doors to be linked to specific buttons - allowing for different doors to be destroyed by different button interactions.

--- Exercise3FPS/Assets/Code/PlayerInteract.cs
+++ Exercise3FPS/Assets/Code/PlayerInteract.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteract : MonoBehaviour
 {
+    public string LinkedName = "Door1";
     private float raycastDist = 1;
-    public LayerMask button;
+    public LayerMask Button;
     public Transform camTrans;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
-            if (Physics.Raycast(camTrans.position, camTrans.forward, out hit, racastDist, button)) {
-                GameObject switch = hit.collider.gameObject;
-                if (switch.CompareTag("Button")) {
-                    Destroy(switch);
+            RaycastHit hit;
+            if (Physics.Raycast(camTrans.position, camTrans.forward, out hit, raycastDist, Button)) {
+                GameObject lever = hit.collider.gameObject;
+                if (lever.CompareTag("Button")) {
+                    GameObject linked = GameObject.Find(LinkedName);
+                    if (linked) {
+                        Destroy(linked.gameObject);
+                    }
                 }
             }
         }
